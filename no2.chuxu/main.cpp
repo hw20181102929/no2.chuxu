5f3d7c96fecca64f7347dcee222cef34badc7fa8
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-06-13 11:45:52 +0800

    Initial Commit

---
+++ no2.chuxu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  no2.chuxu
+//
+//  Created by s20181102929 on 2019/6/13.
+//  Copyright © 2019 s20181102929. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
